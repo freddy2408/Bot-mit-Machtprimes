c5d86db0078ad8f6ee58eec6a430b961890ee009
freddy2408 <frederic.kupka@yahoo.com>
2026-01-20 23:07:34 +0100

    Add files via upload

--- chat.py
+++ chat.py
@@ -142,7 +142,7 @@ def run_survey_and_stop():
 
         # ✅ zum Testen immer Button zeigen
         st.link_button("➡️ Weiter zu Verhandlung 2", get_next_url(PID, ORDER, BOT_VARIANT), use_container_width=True)
-        st.caption("Bitte klicke auf den Button, um zum zweiten Teil zu gelangen.")
+        st.caption("Bitte klicken Sie auf den Button, um zur zweiten Verhandlung zu gelangen.")
 
         st.stop()
     # solange Survey noch nicht abgeschickt: nicht stoppen
